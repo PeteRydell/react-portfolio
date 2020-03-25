3579ad8b1f725f5ab55d17d51f66fd4df8ef305d
Peter Rydell <peterydell@gmail.com>
2020-03-24 23:52:37 -0700

    Updates

---
+++ precache-manifest.a8ab941bdefa8a887592ac3368e13185.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb8b9d714b7121512c8e1d4d766fb9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "430d1bbd17a90eb47c5e",
+    "url": "/static/css/main.b23a4e75.chunk.css"
+  },
+  {
+    "revision": "2b0a6499611fc57bbc10",
+    "url": "/static/js/2.ef481fb0.chunk.js"
+  },
+  {
+    "revision": "a108088b0317a4b3eaee2a12a54f856c",
+    "url": "/static/js/2.ef481fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430d1bbd17a90eb47c5e",
+    "url": "/static/js/main.cd209676.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "bbca896c3f13ca2b373a0ac27d2d794c",
+    "url": "/static/media/BeachRound.bbca896c.png"
+  },
+  {
+    "revision": "7b684373a47ac65d558df7d15282063b",
+    "url": "/static/media/FontLogo.7b684373.png"
+  },
+  {
+    "revision": "e5ecc5a7a246eb87bfe669c99d170790",
+    "url": "/static/media/LARound.e5ecc5a7.png"
+  },
+  {
+    "revision": "003ead4f1c0ebb961290f198adb29c60",
+    "url": "/static/media/Picture1.003ead4f.png"
+  },
+  {
+    "revision": "cbf5ca81d43433143e5564debd5f473f",
+    "url": "/static/media/Picture2.cbf5ca81.png"
+  },
+  {
+    "revision": "6e62b69e5a029f61d8e369c42be2e3ec",
+    "url": "/static/media/Picture3.6e62b69e.png"
+  },
+  {
+    "revision": "50c454accf75137013e99574413a2649",
+    "url": "/static/media/SewaneeRound.50c454ac.png"
+  },
+  {
+    "revision": "0ef94feed50163bba03a466b67f58bbb",
+    "url": "/static/media/TeddyRound.0ef94fee.png"
+  }
+]);
